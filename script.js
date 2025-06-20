d6b642c8e7760bd8b1736e9ae1f2560ea27e2180
J0j0-4951 <jhos3lin3s.z@gmail.com>
2025-06-20 20:12:20 +0000

    added axes

--- script.js
+++ script.js
@@ -2,9 +2,25 @@ function setup() //called once when the program starts
 {
 createCanvas(400,400);
 background(220);
+
 }
 
 function draw() //this runs in a loop to update the display
 {
+    point(150,250);
+    
+
+    point(0,400,0,400)
+    rect(0,0,100,100);
+    rect(100,100,100,100);
+    rect(200,200,100,100);
+    rect(300,300,100,100);
+}
+function drawAxes()
+{
+stroke(0);
+strokeWeight(2);
+line(-width /2, 0, width/2,0);
+line(0,-height/2,0,height/2);
 
 }
